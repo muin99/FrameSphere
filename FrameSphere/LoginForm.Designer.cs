6b1b5cd0077c6e8aadb385591c99247b3e724ba9
muin99 <itsmuin99@gmail.com>
2024-12-28 16:14:09 +0600

    Login Page designed

---
+++ FrameSphere/LoginForm.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace FrameSphere
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.WelcomeLabel = new System.Windows.Forms.Label();
+            this.UserIdLabel = new System.Windows.Forms.Label();
+            this.UserIdTextField = new System.Windows.Forms.TextBox();
+            this.PasswordLabel = new System.Windows.Forms.Label();
+            this.PasswordTextField = new System.Windows.Forms.TextBox();
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.RegisterButton = new System.Windows.Forms.Button();
+            this.AccountLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // WelcomeLabel
+            // 
+            this.WelcomeLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.WelcomeLabel.AutoSize = true;
+            this.WelcomeLabel.BackColor = System.Drawing.Color.Transparent;
+            this.WelcomeLabel.Font = new System.Drawing.Font("Agency FB", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WelcomeLabel.ForeColor = System.Drawing.Color.DarkBlue;
+            this.WelcomeLabel.Location = new System.Drawing.Point(301, 9);
+            this.WelcomeLabel.Name = "WelcomeLabel";
+            this.WelcomeLabel.Size = new System.Drawing.Size(213, 31);
+            this.WelcomeLabel.TabIndex = 0;
+            this.WelcomeLabel.Text = "Welcome To FrameSphere";
+            this.WelcomeLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.WelcomeLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // UserIdLabel
+            // 
+            this.UserIdLabel.AutoSize = true;
+            this.UserIdLabel.Font = new System.Drawing.Font("Agency FB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UserIdLabel.Location = new System.Drawing.Point(181, 158);
+            this.UserIdLabel.Name = "UserIdLabel";
+            this.UserIdLabel.Size = new System.Drawing.Size(56, 20);
+            this.UserIdLabel.TabIndex = 1;
+            this.UserIdLabel.Text = "User ID : ";
+            this.UserIdLabel.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // UserIdTextField
+            // 
+            this.UserIdTextField.Location = new System.Drawing.Point(290, 158);
+            this.UserIdTextField.Name = "UserIdTextField";
+            this.UserIdTextField.Size = new System.Drawing.Size(278, 20);
+            this.UserIdTextField.TabIndex = 2;
+            this.UserIdTextField.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // PasswordLabel
+            // 
+            this.PasswordLabel.AutoSize = true;
+            this.PasswordLabel.Font = new System.Drawing.Font("Agency FB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PasswordLabel.Location = new System.Drawing.Point(181, 192);
+            this.PasswordLabel.Name = "PasswordLabel";
+            this.PasswordLabel.Size = new System.Drawing.Size(72, 20);
+            this.PasswordLabel.TabIndex = 3;
+            this.PasswordLabel.Text = "Password : ";
+            // 
+            // PasswordTextField
+            // 
+            this.PasswordTextField.Location = new System.Drawing.Point(290, 192);
+            this.PasswordTextField.Name = "PasswordTextField";
+            this.PasswordTextField.PasswordChar = '*';
+            this.PasswordTextField.Size = new System.Drawing.Size(278, 20);
+            this.PasswordTextField.TabIndex = 4;
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.Location = new System.Drawing.Point(493, 230);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(75, 23);
+            this.LoginButton.TabIndex = 5;
+            this.LoginButton.Text = "Log in";
+            this.LoginButton.UseVisualStyleBackColor = true;
+            // 
+            // RegisterButton
+            // 
+            this.RegisterButton.Location = new System.Drawing.Point(453, 320);
+            this.RegisterButton.Name = "RegisterButton";
+            this.RegisterButton.Size = new System.Drawing.Size(115, 23);
+            this.RegisterButton.TabIndex = 6;
+            this.RegisterButton.Text = "Register Now";
+            this.RegisterButton.UseVisualStyleBackColor = true;
+            // 
+            // AccountLabel
+            // 
+            this.AccountLabel.AutoSize = true;
+            this.AccountLabel.Location = new System.Drawing.Point(287, 325);
+            this.AccountLabel.Name = "AccountLabel";
+            this.AccountLabel.Size = new System.Drawing.Size(124, 13);
+            this.AccountLabel.TabIndex = 7;
+            this.AccountLabel.Text = "Don\'t Have an account?";
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.AccountLabel);
+            this.Controls.Add(this.RegisterButton);
+            this.Controls.Add(this.LoginButton);
+            this.Controls.Add(this.PasswordTextField);
+            this.Controls.Add(this.PasswordLabel);
+            this.Controls.Add(this.UserIdTextField);
+            this.Controls.Add(this.UserIdLabel);
+            this.Controls.Add(this.WelcomeLabel);
+            this.Name = "LoginForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label WelcomeLabel;
+        private System.Windows.Forms.Label UserIdLabel;
+        private System.Windows.Forms.TextBox UserIdTextField;
+        private System.Windows.Forms.Label PasswordLabel;
+        private System.Windows.Forms.TextBox PasswordTextField;
+        private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.Button RegisterButton;
+        private System.Windows.Forms.Label AccountLabel;
+    }
+}
+
