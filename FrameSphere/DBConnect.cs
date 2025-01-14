3c75135c60468d85eb565fa99fb9794ee004b0a1
Md. Muinul Islam <itsmuin99@gmail.com>
2025-01-14 11:39:42 +0600

    FINAL

--- FrameSphere/DBConnect.cs
+++ FrameSphere/DBConnect.cs
@@ -9,7 +9,7 @@ namespace FrameSphere
 {
     public static class DB
     {
-        private static readonly string ServerName = @"MAZHARUL75\SQLEXPRESS";
+        private static readonly string ServerName = @"ONUKROM";
         private static readonly string ConnectionString = $@"Data Source={ServerName}; Initial Catalog = FrameSphere; Integrated Security = true";
 
         public static SqlConnection Connection = new SqlConnection(ConnectionString);
