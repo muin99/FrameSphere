143407c6b83a02a294867aa5798a24a0fb4a8be0
Md. Muinul Islam <itsmuin99@gmail.com>
2025-01-23 01:04:59 +0600

    vule gesi changes

--- FrameSphere/Event_page.Designer.cs
+++ FrameSphere/Event_page.Designer.cs
@@ -44,16 +44,13 @@ namespace FrameSphere
             this.label9 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.panel3 = new System.Windows.Forms.Panel();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel4 = new System.Windows.Forms.Panel();
+            this.manage = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.cover)).BeginInit();
             this.panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.panel3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.panel1.SuspendLayout();
@@ -80,7 +77,7 @@ namespace FrameSphere
             this.organizer.Multiline = true;
             this.organizer.Name = "organizer";
             this.organizer.ReadOnly = true;
-            this.organizer.Size = new System.Drawing.Size(420, 30);
+            this.organizer.Size = new System.Drawing.Size(305, 87);
             this.organizer.TabIndex = 10;
             this.organizer.Text = "vfvc cvvcc  fgdf dgfrdg fdgdfgbf gh fvb vbxvcv vvcfbgfhfgbcv vb bgv bn gfh bfgnbg" +
     "f bgfbv bbvcbgh fghbgvfbgvbh cvb fghfgbhngv bhg bngfhbbvb b";
@@ -222,28 +219,8 @@ namespace FrameSphere
             this.panel2.Size = new System.Drawing.Size(1266, 89);
             this.panel2.TabIndex = 24;
             // 
-            // pictureBox4
-            // 
-            this.pictureBox4.BackColor = System.Drawing.Color.SteelBlue;
-            this.pictureBox4.Location = new System.Drawing.Point(832, 7);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(406, 421);
-            this.pictureBox4.TabIndex = 3;
-            this.pictureBox4.TabStop = false;
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.BackColor = System.Drawing.Color.SteelBlue;
-            this.pictureBox3.Location = new System.Drawing.Point(420, 7);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(406, 421);
-            this.pictureBox3.TabIndex = 2;
-            this.pictureBox3.TabStop = false;
-            // 
             // panel3
             // 
-            this.panel3.Controls.Add(this.pictureBox4);
-            this.panel3.Controls.Add(this.pictureBox3);
             this.panel3.Controls.Add(this.pictureBox2);
             this.panel3.Location = new System.Drawing.Point(9, 255);
             this.panel3.Name = "panel3";
@@ -273,6 +250,7 @@ namespace FrameSphere
             // panel4
             // 
             this.panel4.BackColor = System.Drawing.Color.LightGreen;
+            this.panel4.Controls.Add(this.manage);
             this.panel4.Controls.Add(this.button2);
             this.panel4.Controls.Add(this.organizer);
             this.panel4.Controls.Add(this.label8);
@@ -290,6 +268,18 @@ namespace FrameSphere
             this.panel4.Size = new System.Drawing.Size(1266, 153);
             this.panel4.TabIndex = 25;
             // 
+            // manage
+            // 
+            this.manage.BackColor = System.Drawing.Color.Red;
+            this.manage.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.manage.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.manage.Location = new System.Drawing.Point(1072, 10);
+            this.manage.Name = "manage";
+            this.manage.Size = new System.Drawing.Size(148, 29);
+            this.manage.TabIndex = 23;
+            this.manage.Text = "Manage ";
+            this.manage.UseVisualStyleBackColor = false;
+            // 
             // Event_page
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -303,8 +293,6 @@ namespace FrameSphere
             ((System.ComponentModel.ISupportInitialize)(this.cover)).EndInit();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.panel3.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.panel1.ResumeLayout(false);
@@ -331,11 +319,10 @@ namespace FrameSphere
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.PictureBox pictureBox4;
-        private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button manage;
     }
 }
