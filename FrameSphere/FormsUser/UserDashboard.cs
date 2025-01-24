c8d1609d2cfe3f205f53714db04a71d61ba4be7f
raisa385 <rr.anwar385@gmail.com>
2025-01-24 20:50:17 +0600

    UserDashboard.cs> social links

--- FrameSphere/FormsUser/UserDashboard.cs
+++ FrameSphere/FormsUser/UserDashboard.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Diagnostics.Tracing;
 using System.Drawing;
 using System.IO;
@@ -385,6 +386,117 @@ namespace FrameSphere
                 this.Hide();
                 ApplyForArtist applyForArtist = new ApplyForArtist();
                 applyForArtist.Show();
+            }
+        }
+
+        private void facebook_pic_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string fb_URL = FSystem.loggedInUser.Facebook;
+                if (fb_URL != null)
+                {
+                    Process.Start(new ProcessStartInfo {
+                        FileName = fb_URL,
+                        UseShellExecute = true
+                    });
+                }
+                else
+                {
+                    Process.Start(new ProcessStartInfo {
+                        FileName = "#",
+                        UseShellExecute = true
+                    });
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred:{ex.Message}");
+            }
+        }
+
+        private void instagram_link_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string insta_URL = FSystem.loggedInUser.Instagram;
+                if (insta_URL != null)
+                {
+                    Process.Start(new ProcessStartInfo {
+                        FileName = insta_URL,
+                        UseShellExecute = true
+                    });
+                }
+                else
+                {
+                    Process.Start(new ProcessStartInfo {
+                        FileName = "#",
+                        UseShellExecute = true
+                    });
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred:{ex.Message}");
+            }
+        }
+
+        private void pinterest_link_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                string pint_URL = FSystem.loggedInUser.Pinterest;
+                if (pint_URL != null)
+                {
+                    Process.Start(new ProcessStartInfo {
+                        FileName = pint_URL,
+                        UseShellExecute = true
+                    });
+                }
+                else
+                {
+                    Process.Start(new ProcessStartInfo {
+                        FileName = "#",
+                        UseShellExecute = true
+                    });
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred:{ex.Message}");
+            }
+
+        }
+
+        private void website_link_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                string web_URL = FSystem.loggedInUser.Website;
+                if (web_URL != null)
+                {
+                    Process.Start(new ProcessStartInfo {
+                        FileName = web_URL,
+                        UseShellExecute = true
+                    });
+                }
+                else
+                {
+                    Process.Start(new ProcessStartInfo {
+                        FileName = "#",
+                        UseShellExecute = true
+                    });
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred:{ex.Message}");
             }
         }
     }
