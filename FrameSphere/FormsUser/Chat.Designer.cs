25de93fa4b212589175611c399cef87c7b01fd95
Md. Muinul Islam <itsmuin99@gmail.com>
2025-02-07 11:53:35 +0600

    chat system complete

--- FrameSphere/FormsUser/Chat.Designer.cs
+++ FrameSphere/FormsUser/Chat.Designer.cs
@@ -1,17 +1,15 @@
-﻿
-namespace FrameSphere.FormsUser
+﻿namespace FrameSphere.FormsUser
 {
     partial class Chat
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label nameuser;
+        private System.Windows.Forms.FlowLayoutPanel msgpanel;
+        private System.Windows.Forms.Button sendbutton;
+        private System.Windows.Forms.RichTextBox message;
+        private System.Windows.Forms.Panel recentUsersPanel;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -21,12 +19,6 @@ namespace FrameSphere.FormsUser
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             this.panel2 = new System.Windows.Forms.Panel();
@@ -34,67 +26,93 @@ namespace FrameSphere.FormsUser
             this.msgpanel = new System.Windows.Forms.FlowLayoutPanel();
             this.sendbutton = new System.Windows.Forms.Button();
             this.message = new System.Windows.Forms.RichTextBox();
+            this.recentUsersPanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.ForestGreen;
+            this.panel2.Controls.Add(this.label1);
             this.panel2.Controls.Add(this.nameuser);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel2.Location = new System.Drawing.Point(0, 0);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(600, 46);
-            this.panel2.TabIndex = 1;
+            this.panel2.Size = new System.Drawing.Size(800, 50);
+            this.panel2.TabIndex = 4;
             // 
             // nameuser
             // 
             this.nameuser.AutoSize = true;
-            this.nameuser.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.nameuser.Location = new System.Drawing.Point(270, 11);
+            this.nameuser.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
+            this.nameuser.Location = new System.Drawing.Point(350, 13);
             this.nameuser.Name = "nameuser";
-            this.nameuser.Size = new System.Drawing.Size(66, 24);
+            this.nameuser.Size = new System.Drawing.Size(0, 24);
             this.nameuser.TabIndex = 0;
-            this.nameuser.Text = "label1";
             // 
             // msgpanel
             // 
+            this.msgpanel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.msgpanel.AutoScroll = true;
             this.msgpanel.BackColor = System.Drawing.Color.LightGreen;
             this.msgpanel.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
-            this.msgpanel.Location = new System.Drawing.Point(0, 46);
+            this.msgpanel.Location = new System.Drawing.Point(200, 50);
             this.msgpanel.Name = "msgpanel";
-            this.msgpanel.Size = new System.Drawing.Size(600, 543);
-            this.msgpanel.TabIndex = 1;
+            this.msgpanel.Padding = new System.Windows.Forms.Padding(50, 50, 50, 60);
+            this.msgpanel.Size = new System.Drawing.Size(600, 514);
+            this.msgpanel.TabIndex = 2;
+            this.msgpanel.WrapContents = false;
             // 
             // sendbutton
             // 
             this.sendbutton.BackColor = System.Drawing.Color.LightGreen;
             this.sendbutton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.sendbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.sendbutton.Location = new System.Drawing.Point(514, 618);
+            this.sendbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.sendbutton.Location = new System.Drawing.Point(720, 570);
             this.sendbutton.Name = "sendbutton";
-            this.sendbutton.Size = new System.Drawing.Size(75, 23);
-            this.sendbutton.TabIndex = 5;
+            this.sendbutton.Size = new System.Drawing.Size(75, 30);
+            this.sendbutton.TabIndex = 0;
             this.sendbutton.Text = "Send";
             this.sendbutton.UseVisualStyleBackColor = false;
             this.sendbutton.Click += new System.EventHandler(this.sendbutton_Click_1);
             // 
             // message
             // 
-            this.message.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.message.Location = new System.Drawing.Point(0, 595);
+            this.message.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.message.Location = new System.Drawing.Point(200, 570);
             this.message.Name = "message";
-            this.message.Size = new System.Drawing.Size(600, 69);
-            this.message.TabIndex = 4;
+            this.message.Size = new System.Drawing.Size(500, 30);
+            this.message.TabIndex = 1;
             this.message.Text = "";
+            // 
+            // recentUsersPanel
+            // 
+            this.recentUsersPanel.AutoScroll = true;
+            this.recentUsersPanel.BackColor = System.Drawing.Color.LightGray;
+            this.recentUsersPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.recentUsersPanel.Location = new System.Drawing.Point(0, 50);
+            this.recentUsersPanel.Name = "recentUsersPanel";
+            this.recentUsersPanel.Size = new System.Drawing.Size(200, 550);
+            this.recentUsersPanel.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(365, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Chat Box";
             // 
             // Chat
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(601, 663);
+            this.ClientSize = new System.Drawing.Size(800, 600);
             this.Controls.Add(this.sendbutton);
             this.Controls.Add(this.message);
             this.Controls.Add(this.msgpanel);
+            this.Controls.Add(this.recentUsersPanel);
             this.Controls.Add(this.panel2);
             this.Name = "Chat";
             this.Text = "Chat";
@@ -104,11 +122,6 @@ namespace FrameSphere.FormsUser
 
         }
 
-        #endregion
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Label nameuser;
-        private System.Windows.Forms.FlowLayoutPanel msgpanel;
-        private System.Windows.Forms.Button sendbutton;
-        private System.Windows.Forms.RichTextBox message;
+        private System.Windows.Forms.Label label1;
     }
 }
--- FrameSphere/FormsUser/Chat.cs
+++ FrameSphere/FormsUser/Chat.cs
@@ -16,6 +16,7 @@ namespace FrameSphere.FormsUser
         {
             this.you = you;
             InitializeComponent();
+            LoadRecentUsers();
             LoadMessages();
         }
 
@@ -24,11 +25,10 @@ namespace FrameSphere.FormsUser
             string messageText = message.Text.Trim();
             if (string.IsNullOrEmpty(messageText)) return;
 
-            // Save to database
             SaveMessageToDatabase(me.UserName, you.UserName, messageText);
 
-            // Update UI
-            AddMessageToPanel(messageText, true);
+            // Fixed: Added DateTime.Now as the third argument
+            AddMessageToPanel(messageText, true, DateTime.Now);
             message.Clear();
         }
 
@@ -37,7 +37,7 @@ namespace FrameSphere.FormsUser
             using (SqlConnection conn = DB.Connect())
             {
                 conn.Open();
-                string query = "INSERT INTO Messages (sender_id, receiver_id, message) VALUES (@sender, @receiver, @message)";
+                string query = "INSERT INTO Messages (sender_id, receiver_id, message, sent_at) VALUES (@sender, @receiver, @message, GETDATE())";
                 using (SqlCommand cmd = new SqlCommand(query, conn))
                 {
                     cmd.Parameters.AddWithValue("@sender", senderId);
@@ -54,7 +54,9 @@ namespace FrameSphere.FormsUser
             using (SqlConnection conn = DB.Connect())
             {
                 conn.Open();
-                string query = "SELECT sender_id, message FROM Messages WHERE (sender_id = @me AND receiver_id = @you) OR (sender_id = @you AND receiver_id = @me) ORDER BY sent_at";
+                string query = "SELECT sender_id, message, sent_at FROM Messages " +
+                               "WHERE (sender_id = @me AND receiver_id = @you) OR (sender_id = @you AND receiver_id = @me) " +
+                               "ORDER BY sent_at";
                 using (SqlCommand cmd = new SqlCommand(query, conn))
                 {
                     cmd.Parameters.AddWithValue("@me", me.UserName);
@@ -64,15 +66,84 @@ namespace FrameSphere.FormsUser
                         while (reader.Read())
                         {
                             bool isMe = reader["sender_id"].ToString() == me.UserName;
-                            AddMessageToPanel(reader["message"].ToString(), isMe);
+                            DateTime sentAt = Convert.ToDateTime(reader["sent_at"]);
+                            AddMessageToPanel(reader["message"].ToString(), isMe, sentAt);
                         }
                     }
                 }
             }
         }
 
-        private void AddMessageToPanel(string messageText, bool isMe)
+        private void LoadRecentUsers()
         {
+            recentUsersPanel.Controls.Clear();
+            using (SqlConnection conn = DB.Connect())
+            {
+                conn.Open();
+                string query = @"
+                SELECT user_name, last_message_time FROM (
+                    SELECT 
+                        CASE 
+                            WHEN sender_id = @me THEN receiver_id 
+                            ELSE sender_id 
+                        END AS user_name, 
+                        MAX(sent_at) AS last_message_time
+                    FROM Messages
+                    WHERE sender_id = @me OR receiver_id = @me
+                    GROUP BY 
+                        CASE 
+                            WHEN sender_id = @me THEN receiver_id 
+                            ELSE sender_id 
+                        END
+                ) AS RecentUsers
+                ORDER BY last_message_time";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@me", me.UserName);
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string userName = reader["user_name"].ToString();
+                            DateTime lastMessageTime = Convert.ToDateTime(reader["last_message_time"]);
+                            string timeAgo = GetTimeAgo(lastMessageTime);
+
+                            AddUserToRecentPanel(userName, timeAgo);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void AddUserToRecentPanel(string userName, string timeAgo)
+        {
+            Button userButton = new Button {
+                Text = $"{userName} ({timeAgo})",
+                Dock = DockStyle.Top,
+                Height = 40,
+                BackColor = Color.LightGray,
+                FlatStyle = FlatStyle.Flat
+            };
+
+            // Fixed: Implemented missing OpenChatWithUser method
+          
+            userButton.Click += (sender, e) => OpenChatWithUser(userName);
+            recentUsersPanel.Controls.Add(userButton);
+        }
+
+        private void AddMessageToPanel(string messageText, bool isMe, DateTime sentAt)
+        {
+            // Time label setup
+            Label timeLabel = new Label {
+                Text = sentAt.ToString("hh:mm tt"),
+                AutoSize = true,
+                ForeColor = Color.Gray,
+                Font = new Font("Arial", 8, FontStyle.Italic),
+                Margin = new Padding(5)
+            };
+
+            // Message label setup
             Label messageLabel = new Label {
                 Text = messageText,
                 AutoSize = true,
@@ -81,39 +152,67 @@ namespace FrameSphere.FormsUser
                 BackColor = isMe ? Color.LightBlue : Color.LightGray,
                 ForeColor = Color.Black,
                 Font = new Font("Arial", 10),
-                Margin = new Padding(5),
+                Margin = new Padding(5)
             };
 
+            // Container setup
             FlowLayoutPanel container = new FlowLayoutPanel {
                 AutoSize = true,
-                FlowDirection = FlowDirection.LeftToRight,
+                FlowDirection = isMe ? FlowDirection.RightToLeft : FlowDirection.LeftToRight,
                 Dock = DockStyle.Top,
                 Padding = new Padding(5),
-                Width = msgpanel.Width - 20
+                Width = msgpanel.Width - 20 // Adjust width to fit within msgpanel
             };
 
+            // Message container (holds time and message)
+            FlowLayoutPanel messageContainer = new FlowLayoutPanel {
+                AutoSize = true,
+                FlowDirection = FlowDirection.TopDown
+                
+            };
+ 
+
+
+            // Add time and message labels to the message container
+            messageContainer.Controls.Add(timeLabel);
+            messageContainer.Controls.Add(messageLabel);
+
+
+            // Add message container to the main container
+            container.Controls.Add(messageContainer);
+
+            // Add padding to messages from 'me' for proper alignment
             if (isMe)
             {
-                container.Controls.Add(new Label { Width = container.Width / 2 }); // Push to the right
-                container.Controls.Add(messageLabel);
-
-            }
-            else
-            {
-                container.Controls.Add(messageLabel);
+                // Add space for messages from 'me' on the left side
+                container.Controls.Add(new Label { Width = container.Width / 2 });
             }
 
+            // Add the container to the message panel
             msgpanel.Controls.Add(container);
             msgpanel.ScrollControlIntoView(container);
         }
-        private void message_Enter(object sender, EventArgs e)
-        {
 
+
+        private string GetTimeAgo(DateTime lastMessageTime)
+        {
+            TimeSpan timeSpan = DateTime.Now - lastMessageTime;
+
+            if (timeSpan.TotalSeconds < 60)
+                return $"{timeSpan.Seconds} secs ago";
+            if (timeSpan.TotalMinutes < 60)
+                return $"{timeSpan.Minutes} mins ago";
+            if (timeSpan.TotalHours < 24)
+                return $"{timeSpan.Hours} hrs ago";
+            return $"{timeSpan.Days} days ago";
         }
 
-        private void message_Leave(object sender, EventArgs e)
+        // Fixed: Implemented the missing OpenChatWithUser method
+        private void OpenChatWithUser(string userName)
         {
-
+            this.Hide();
+            Chat newChat = new Chat(new User(userName)); // Assuming `User` class has a UserName property
+            newChat.Show();
         }
     }
 }
